1d04788f64188dd94c07fa6631a5982a9c67a96b
sohag502 <sohag000055@gmail.com>
2021-06-17 10:20:12 +0600

    fetch data by ajax in dropdown

--- resources/views/backend/topic_details/create.blade.php
+++ resources/views/backend/topic_details/create.blade.php
@@ -3,123 +3,160 @@
 @section('content')
 
 
-        <!-- Content Header (Page header) -->
+    <!-- Content Header (Page header) -->
     <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Topices details</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Add Topices Details</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0 text-dark">Topices details</h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active">Add Topices Details</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
     </div>
     <!-- /.content-header -->
 
     <!-- Main content -->
     <section class="content">
-      <div class="container-fluid">
-        <!-- Small boxes (Stat box) -->
+        <div class="container-fluid">
+            <!-- Small boxes (Stat box) -->
 
-        <!-- /.row -->
-        <!-- Main row -->
-  
-<section class="content">
-      <div class="row">
-        <div class="col-12">
+            <!-- /.row -->
+            <!-- Main row -->
 
-          <div class="card">
-            <div class="card-header">
-              <h2 class="card-title"> Add Topices Details</h2>
-              {{-- <a href="{{ route('top_de.list',$topices->topices_title_slug) }}"><h4 class="btn btn-sm btn-success float-right"><i class="fa fa-list">All Topices Details List</i></h4></a> --}}
-            </div>
-            <!-- /.card-header -->
-            <div class="card-body">
-              <form action="{{ route('top_de.store')}}" method="POST" enctype="multipart/form-data">
-                @csrf
-
+            <section class="content">
                 <div class="row">
-                  <div class="col-md-12">
-                      <label class="text-info" for="topices_slug">Select Your Topics Type</label>
-                      <select name="topices_slug" class="form-control select2 select2-info" data-dropdown-css-class="select2-info" style="width: 100%;">
-                          @foreach ($topices_type as $row)
-                              <option value="{{ $row->topics_slug}}">{{ $row->topics_type}}</option>
-                          @endforeach
-                      </select>
-                  </div>
-                </div>
+                    <div class="col-12">
 
-                <div class="row">
-                    <div class="col-md-12">
-                        <label class="text-info" for="topic_id">Select Your Topics</label>
-                        <select name="topic_id" class="form-control select2 select2-info" data-dropdown-css-class="select2-info" style="width: 100%;">
-                            @foreach ($topices as $row)
-                                <option value="{{ $row->topices_title_slug}}">{{ $row->topices_title}}</option>
-                            @endforeach
-                        </select>
+                        <div class="card">
+                            <div class="card-header">
+                                <h2 class="card-title"> Add Topices Details</h2>
+                                {{-- <a href="{{ route('top_de.list',$topices->topices_title_slug) }}"><h4 class="btn btn-sm btn-success float-right"><i class="fa fa-list">All Topices Details List</i></h4></a> --}}
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <form action="{{ route('top_de.store') }}" method="POST" enctype="multipart/form-data">
+                                    @csrf
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <label class="text-info" for="topices_slug">Select Your Topics Type</label>
+                                            <select id="topic_type" name="topices_slug"
+                                                class="form-control select2 select2-info"
+                                                data-dropdown-css-class="select2-info" style="width: 100%;">
+                                                <option value="" selected="" disabled="">Select Topics Type</option>
+                                                @foreach ($topices_type as $row)
+                                                    <option value="{{ $row->topics_slug }}">{{ $row->topics_type }}
+                                                    </option>
+                                                @endforeach
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <label class="text-info" for="topic_id">Select Your Topics</label>
+                                            <select name="topic_id" id="topic" class="form-control select2 select2-info"
+                                                data-dropdown-css-class="select2-info" style="width: 100%;">
+                                                {{-- @foreach ($topices as $row)
+                                                    <option value="{{ $row->topices_title_slug }}">
+                                                        {{ $row->topices_title }}</option>
+                                                @endforeach --}}
+                                            </select>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            {{-- use summernote plugin --}}
+                                            <label for="description" class="text-info">Description</label>
+                                            <textarea class="textarea" id="summernote" name="description" rows="6"
+                                                placeholder="Place some text here"
+                                                style="width: 100%; height: 400px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">
+                                                                                                                                                {{ old('description') }}
+                                                                                                                                        </textarea>
+
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <label class="text-info" for="file">File</label>
+                                            <input type="file" class="form-control" value="{{ old('file') }}" name="file">
+                                            @error('file')
+                                                <strong class="text-danger">{{ $message }} </strong>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <br>
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <label class="text-info" for="video_path">Video Path</label>
+                                            <input type="file" class="form-control" value="{{ old('video_path') }}"
+                                                name="video_path" placeholder="Enter Video path:">
+                                            @error('video_path')
+                                                <strong class="text-danger">{{ $message }} </strong>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <br>
+
+                                    <input type="hidden" name="created_by" value="{{ Auth::id() }}">
+
+                                    <!-- /.card-body -->
+                                    <div class="card-footer">
+                                        <button type="submit" id="butsave" class="btn btn-primary">Add Details</button>
+                                    </div>
+                            </div>
+                            </form>
+                        </div>
+                        <!-- /.card -->
                     </div>
+                    <!-- /.col -->
                 </div>
-               
-                <div class="row">
-                    <div class="col-md-12">
-                        {{-- use summernote plugin --}}
-                        <label for="description" class="text-info">Description</label>
-                        <textarea class="textarea" id="summernote" name="description" rows="6" placeholder="Place some text here"
-                                  style="width: 100%; height: 400px; font-size: 14px; line-height: 18px; border: 1px solid #dddddd; padding: 10px;">
-                                {{old('description')}}
-                        </textarea>
-                       
-                    </div>
-                </div>
-
-                <div class="row">
-                    <div class="col-md-12">
-                        <label class="text-info" for="file">File</label>
-                        <input type="file" class="form-control" value="{{ old('file')}}" name="file">
-                        @error('file')
-                        <strong class="text-danger">{{ $message }} </strong>
-                        @enderror  
-                    </div>
-                </div>
-                <br>
-                <div class="row">
-                    <div class="col-md-12">
-                        <label class="text-info" for="video_path">Video Path</label>
-                        <input type="file" class="form-control" value="{{ old('video_path')}}" name="video_path" placeholder="Enter Video path:">
-                        @error('video_path')
-                        <strong class="text-danger">{{ $message }} </strong>
-                        @enderror  
-                    </div>
-                </div>
-                <br>
-                
-                <input type="hidden" name="created_by" value="{{ Auth::id() }}">
-             
-            <!-- /.card-body -->
-            <div class="card-footer">
-                  <button type="submit" id="butsave" class="btn btn-primary">Add Details</button>
-            </div>
-            </div>
-          </form>
-          </div>
-          <!-- /.card -->
-        </div>
-        <!-- /.col -->
-      </div>
-      <!-- /.row -->
-    </section>
-          <!-- /.Left col -->
+                <!-- /.row -->
+            </section>
+            <!-- /.Left col -->
 
         </div>
 
         <!-- /.row (main row) -->
-      </div><!-- /.container-fluid -->
+        </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
+
+    <script>
+        $(document).on('change', '#topic_type', function() {
+
+            var topic_type = $(this).val();
+            $.ajax({
+                url: "/topices/get-topics",
+                data: {
+                    topic_type: topic_type
+                },
+                success: function(data) {
+
+                    var html = '<option value="" selected="" disabled="">Select Topics</option>'
+                    $.each(data, function(key, v) {
+                        html += '<option value="' + v.topices_title_slug + '">' + v
+                            .topices_title +
+                            '</option>'
+                    })
+                    $('#topic').html(html)
+
+
+
+
+                }
+            })
+
+        })
+
+    </script>
 
 @endsection
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -10,33 +10,38 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="shortcut icon" href="" />
 
-     <!-- Font Awesome -->
-  <link rel="stylesheet" href="{{asset('assets/backend')}}/plugins/fontawesome-free/css/all.min.css">
-  <!-- Ionicons -->
-  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-  <!-- Tempusdominus Bbootstrap 4 -->
-  <link rel="stylesheet" href="{{asset('assets/backend')}}/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
-  <!-- iCheck -->
-  <link rel="stylesheet" href="{{asset('assets/backend')}}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
-  <!-- JQVMap -->
-  <link rel="stylesheet" href="{{asset('assets/backend')}}/plugins/jqvmap/jqvmap.min.css">
-  <!-- Theme style -->
-  <link rel="stylesheet" href="{{asset('assets/backend')}}/dist/css/adminlte.min.css">
-  <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="{{asset('assets/backend')}}/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
-  <!-- Daterange picker -->
-  <link rel="stylesheet" href="{{asset('assets/backend')}}/plugins/daterangepicker/daterangepicker.css">
-  <!-- summernote -->
-  <link rel="stylesheet" href="{{asset('assets/backend')}}/plugins/summernote/summernote-bs4.css">
-  <!-- Select2 -->
-  <link rel="stylesheet" href="{{ asset('assets/backend') }}/plugins/select2/css/select2.min.css">
-  <link rel="stylesheet" href="{{ asset('assets/backend') }}/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
-   <!-- toastr -->
-  <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{ asset('assets/backend') }}/plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bbootstrap 4 -->
+    <link rel="stylesheet"
+        href="{{ asset('assets/backend') }}/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <!-- iCheck -->
+    <link rel="stylesheet" href="{{ asset('assets/backend') }}/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <!-- JQVMap -->
+    <link rel="stylesheet" href="{{ asset('assets/backend') }}/plugins/jqvmap/jqvmap.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{ asset('assets/backend') }}/dist/css/adminlte.min.css">
+    <!-- overlayScrollbars -->
+    <link rel="stylesheet"
+        href="{{ asset('assets/backend') }}/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <!-- Daterange picker -->
+    <link rel="stylesheet" href="{{ asset('assets/backend') }}/plugins/daterangepicker/daterangepicker.css">
+    <!-- summernote -->
+    <link rel="stylesheet" href="{{ asset('assets/backend') }}/plugins/summernote/summernote-bs4.css">
+    <!-- Select2 -->
+    <link rel="stylesheet" href="{{ asset('assets/backend') }}/plugins/select2/css/select2.min.css">
+    <link rel="stylesheet"
+        href="{{ asset('assets/backend') }}/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+    <!-- toastr -->
+    <link rel="stylesheet" href="http://cdn.bootcss.com/toastr.js/latest/css/toastr.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
-   
+    <script src="{{ asset('assets/backend') }}/plugins/jquery/jquery.min.js"></script>
+
+
 
     <style type="text/css">
         .select2-selection {
@@ -270,40 +275,44 @@
     @endif
 
 
-        <!-- jQuery -->
-    <script src="{{ asset('assets/backend')}}/plugins/jquery/jquery.min.js"></script>
+    <!-- jQuery -->
+
     <!-- jQuery UI 1.11.4 -->
-    <script src="{{ asset('assets/backend')}}/plugins/jquery-ui/jquery-ui.min.js"></script>
+    <script src="{{ asset('assets/backend') }}/plugins/jquery-ui/jquery-ui.min.js"></script>
     <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
     <script>
-    $.widget.bridge('uibutton', $.ui.button)
+        $.widget.bridge('uibutton', $.ui.button)
+
     </script>
     <!-- Bootstrap 4 -->
-    <script src="{{ asset('assets/backend')}}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="{{ asset('assets/backend') }}/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- ChartJS -->
-    <script src="{{ asset('assets/backend')}}/plugins/chart.js/Chart.min.js"></script>
+    <script src="{{ asset('assets/backend') }}/plugins/chart.js/Chart.min.js"></script>
     <!-- Sparkline -->
-    <script src="{{ asset('assets/backend')}}/plugins/sparklines/sparkline.js"></script>
+    <script src="{{ asset('assets/backend') }}/plugins/sparklines/sparkline.js"></script>
     <!-- JQVMap -->
-    <script src="{{ asset('assets/backend')}}/plugins/jqvmap/jquery.vmap.min.js"></script>
-    <script src="{{ asset('assets/backend')}}/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+    <script src="{{ asset('assets/backend') }}/plugins/jqvmap/jquery.vmap.min.js"></script>
+    <script src="{{ asset('assets/backend') }}/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
     <!-- jQuery Knob Chart -->
-    <script src="{{ asset('assets/backend')}}/plugins/jquery-knob/jquery.knob.min.js"></script>
+    <script src="{{ asset('assets/backend') }}/plugins/jquery-knob/jquery.knob.min.js"></script>
     <!-- daterangepicker -->
-    <script src="{{ asset('assets/backend')}}/plugins/moment/moment.min.js"></script>
-    <script src="{{ asset('assets/backend')}}/plugins/daterangepicker/daterangepicker.js"></script>
+    <script src="{{ asset('assets/backend') }}/plugins/moment/moment.min.js"></script>
+    <script src="{{ asset('assets/backend') }}/plugins/daterangepicker/daterangepicker.js"></script>
     <!-- Tempusdominus Bootstrap 4 -->
-    <script src="{{ asset('assets/backend')}}/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script
+        src="{{ asset('assets/backend') }}/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js">
+    </script>
     <!-- Summernote -->
-    <script src="{{ asset('assets/backend')}}/plugins/summernote/summernote-bs4.min.js"></script>
+    <script src="{{ asset('assets/backend') }}/plugins/summernote/summernote-bs4.min.js"></script>
     <!-- overlayScrollbars -->
-    <script src="{{ asset('assets/backend')}}/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+    <script src="{{ asset('assets/backend') }}/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js">
+    </script>
     <!-- AdminLTE App -->
-    <script src="{{ asset('assets/backend')}}/dist/js/adminlte.js"></script>
+    <script src="{{ asset('assets/backend') }}/dist/js/adminlte.js"></script>
     <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
-    <script src="{{ asset('assets/backend')}}/dist/js/pages/dashboard.js"></script>
+    <script src="{{ asset('assets/backend') }}/dist/js/pages/dashboard.js"></script>
     <!-- AdminLTE for demo purposes -->
-    <script src="{{ asset('assets/backend')}}/dist/js/demo.js"></script>
+    <script src="{{ asset('assets/backend') }}/dist/js/demo.js"></script>
     <!-- select 2 -->
     <script src="{{ asset('assets/backend') }}/plugins/select2/js/select2.full.min.js"></script>
 
@@ -311,15 +320,16 @@
     {!! Toastr::message() !!}
 
 
-   
 
-    
-    
+
+
+
     <script>
         $(document).ready(function() {
             $('#summernote').summernote();
         });
-      </script>
+
+    </script>
 
     <script>
         $(function() {
@@ -333,7 +343,7 @@
 
             //Initialize Select2 Elements
             $('.select2bs4').select2({
-            theme: 'bootstrap4'
+                theme: 'bootstrap4'
             })
 
         });
@@ -390,31 +400,7 @@
 
     </script>
 
-    <script>
-        $(document).ready(function() {
 
-            $(document).on('keyup', '#search_form', function() {
-                var search = $(this).val();
-                var auth_user_id = $('#auth_user_id').val();
-                $.ajax({
-                    method: 'POST',
-                    data: {
-                        search: search,
-                        auth_user_id: auth_user_id,
-                    },
-                    url: '/api/gig/search',
-                    beforeSend: function() {
-
-                    },
-                    success: function(data) {
-                        $('#gig_content').html(data)
-                    }
-                })
-            })
-
-        })
-
-    </script>
 
     <script>
         $.ajaxSetup({
